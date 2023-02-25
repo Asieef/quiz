2be4075fcfec407793a83331620a389e33d42d8a
Asieef <asieef.chowdhury@gmail.com>
2023-02-25 11:56:10 +0600

    first

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import Card from "./components/Card.vue";
+import q from "./data/quizes.json";
+import { ref, watch } from "vue";
+
+const quizes = ref(q);
+const search = ref("");
+
+watch(search, () => {
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
+</script>
+
+<template>
+  <div class="mt-12 container mx-auto">
+    <!-- Header Search -->
+    <div class="flex gap-4 mx-12 items-center">
+      <h2 class="text-4xl font-bold">Quizes</h2>
+
+      <form>
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >Search</label
+        >
+        <div class="relative">
+          <div
+            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+          >
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            v-model.trim="search"
+            type="search"
+            id="default-search"
+            class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search..."
+            required
+          />
+        </div>
+      </form>
+    </div>
+    <Card :quizes="quizes" />
+  </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { quizes } = defineProps(["quizes"]);
+</script>
+
+<template>
+  <div class="mt-12 mx-12 grid grid-cols-4 gap-4">
+    <div
+      v-for="quiz in quizes"
+      :key="quiz.id"
+      class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 col-span-1"
+    >
+      <a href="#">
+        <img class="rounded-t-lg" :src="quiz.img" :alt="quiz.name" />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            {{ quiz.name }}
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {{ quiz.questions.length }} Questions
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
